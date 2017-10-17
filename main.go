cf6292ad4bb8fc9739ed2f977a1f065f0df4d646
erwanlbp <erwan.lbp@gmail.com>
2017-10-17 16:12:32 +0200

    gui

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"flag"
-
 	"fmt"
+	"math/rand"
+	"os"
 	"time"
 
-	"github.com/erwanlbp/gameOfLife/data"
-	"math/rand"
+	"github.com/andlabs/ui"
+	"github.com/erwanlbp/game-of-life/data"
 )
 
 var nbLig, nbCol int
@@ -19,6 +20,9 @@ func init() {
 }
 
 func main() {
+	guiTEST()
+	os.Exit(0)
+
 	rand.Seed(time.Now().Unix())
 
 	grid := data.InitGrid(nbLig, nbCol)
@@ -38,10 +42,36 @@ func main() {
 	for i := 0; ; i++ {
 		grid.Iterate()
 		grid.Print()
-		fmt.Print("Gen.",i)
+		fmt.Print("Gen.", i)
 		time.Sleep(time.Millisecond * 200)
 	}
 
 	elapsed := time.Since(start)
-	fmt.Println("END : ",elapsed)
+	fmt.Println("END : ", elapsed)
+}
+
+func guiTEST() {
+	err := ui.Main(func() {
+		name := ui.NewEntry()
+		button := ui.NewButton("Greet")
+		greeting := ui.NewLabel("")
+		box := ui.NewVerticalBox()
+		box.Append(ui.NewLabel("Enter your name:"), false)
+		box.Append(name, false)
+		box.Append(button, false)
+		box.Append(greeting, false)
+		window := ui.NewWindow("Hello", 200, 100, false)
+		window.SetChild(box)
+		button.OnClicked(func(*ui.Button) {
+			greeting.SetText("Hello, " + name.Text() + "!")
+		})
+		window.OnClosing(func(*ui.Window) bool {
+			ui.Quit()
+			return true
+		})
+		window.Show()
+	})
+	if err != nil {
+		panic(err)
+	}
 }
